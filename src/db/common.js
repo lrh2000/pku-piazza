200129f555ad8be475e6b615eda9635f64311d57
PanJason <pyyjason@gmail.com>
2021-05-26 11:48:46 +0800

    Modify the URL in src/db/common.js and Add src/db/homework.js.

--- src/db/common.js
+++ src/db/common.js
@@ -1,4 +1,4 @@
 import { createPool } from "slonik";
 
-const uri = "postgres://postgres@127.0.0.1:5432/postgres";
+const uri = "postgres://pyyjason:ppyy213%@localhost:5432/piazza";
 export const pool = createPool(uri);
---
+++ src/db/homework.js
@@ -0,0 +1,26 @@
+import { pool } from "./common";
+import { ConnectionError, sql } from "slonik";
+
+export async function getHomeworkList(){
+    const result = await pool.connect(async (connection) =>{
+        const data = await connection.query(sql`SELECT * FROM homework`);
+        return data;
+    }
+    );
+    return result.rows;
+}
+
+export async function getHomeworkByCID(cid) {
+    if (typeof cid !== "number"){return null;}
+    if (isNaN(cid)) {return null;}
+    const result = await pool.connect( async (connection) => {
+        const data = await connection.query(
+            sql`SELECT * FROM homework WHERE courseid = ${cid}`
+        );
+        return data;
+    } 
+    );
+    const rows = result.rows;
+    if (rows.length === 0 ){return null;}
+    return rows;
+}
